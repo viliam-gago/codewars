9a8befc1b0e5d28a4113a04c9a3650dd8ac35bb5
Viliam Gago <viliam.gago@seznam.cz>
2020-01-24 08:02:09 +0100

    Posledni aktualizace - vyuziti list comperhension

--- array.diff.py
+++ array.diff.py
@@ -1,8 +1,5 @@
 def array_diff(a, b):
 
-    for numberb in b:
-        while numberb in a:
-            a.remove(numberb)
-    return a
+    return [x for x in a if x not in b]
 
 print(array_diff([1,2,2,2,2,2,3],[2]))
