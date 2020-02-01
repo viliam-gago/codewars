c9cc6e0b75e768c4b4c5b3f8b4858b4581529182
Viliam Gago <viliam.gago@seznam.cz>
2020-02-01 15:48:24 +0100

    Practice: turning the order of letters in words longer than 5. (mala uprava)

--- Stop gninnipS My sdroW!.py
+++ Stop gninnipS My sdroW!.py
@@ -1,9 +1,9 @@
 def spin_words(sentence):
     words = sentence.split()
-    new_words = []
-
     nw = ' '.join([word[-1::-1] if len(word) >= 5 else word for word in words])
     return nw
+
+
     # for word in words:
     #     word = list(word)
     #     if len(word) >= 5 :
