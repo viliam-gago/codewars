651194953a0d38c733d0f5d4cad5011953b793c5
Viliam Gago <viliam.gago@seznam.cz>
2020-02-02 14:44:41 +0100

    shorter version

--- ten minute walk.py
+++ ten minute walk.py
@@ -2,16 +2,6 @@ def is_valid_walk(walk):
 
     directions = {'n': -11, 's': 11, 'w': -1, 'e': 1}
     track = [directions[i] for i in walk]
-
-    if len(walk) == 10 and sum(track) == 0:
-        return True
-
-
-    else:
-        return False
-
-
-
-
+    return len(walk) == 10 and sum(track) == 0
 
 print(is_valid_walk(['w', 'w', 's', 'n', 'e', 'e', 'n', 's']))
